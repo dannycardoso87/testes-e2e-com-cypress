f08f5ce55a378e1fe3e7ec30f4c203b23a34a4c0
dannycardoso87 <danny.cardoso87@gmail.com>
2024-02-28 23:51:19 -0500

    Change the baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
-    //baseUrl: 'https://app.proposify.com',
-    baseUrl: 'https://notes-serverless-app.com',
+    baseUrl: 'https://app.proposify.com',
+    //baseUrl: 'https://notes-serverless-app.com',
     env: {
       viewportWidthBreakpoint: 768,
     },
